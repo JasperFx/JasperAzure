6009363b41e3703d901aa097eb8d8b7ba29d8368
jeremydmiller <jeremydmiller@yahoo.com>
2019-10-21 13:30:01 -0500

    initial import

---
+++ src/Jasper.ApplicationInsights/ApplicationInsightsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Jasper.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace Jasper.ApplicationInsights
+{
+    public class ApplicationInsightsSettings
+    {
+        public string InstrumentationKey { get; set; } = "Jasper";
+    }
+
+
+}
